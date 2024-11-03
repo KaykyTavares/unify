47af9dc14e5580b709249ef36058d06e43f1df93
Kayky Tavares <kayky.programmer@gmail.com>
2024-11-03 19:43:27 -0300

    First commit

---
+++ unifyWebApp/src/main/java/com/unify/unifyWebApp/UnifyWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.unify.unifyWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnifyWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnifyWebAppApplication.class, args);
+	}
+
+}
---
+++ unifyWebApp/src/test/java/com/unify/unifyWebApp/UnifyWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unify.unifyWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnifyWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
